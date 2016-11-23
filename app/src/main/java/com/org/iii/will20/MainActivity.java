ad59df645e55dd1c99ac7a2ec86bcfa014fb1842
Will <leo0908w@yahoo.com.tw>
2016-11-23 10:11:53 +0800

    v1

---
+++ app/src/main/java/com/org/iii/will20/MainActivity.java
@@ -0,0 +1,44 @@
+package com.org.iii.will20;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TelephonyManager tmgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    123);
+        }else{
+            init();
+        }
+    }
+
+    private void init() {
+        tmgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        Log.v("will", "imi" + tmgr.getDeviceId());
+        Log.v("will", "sim" + tmgr.getSubscriberId());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+}
